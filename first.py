5a1aa46062a73abfd323e8d51e154b968b672945
BandosRodriges <47812180+BandosRodriges@users.noreply.github.com>
2019-02-20 16:41:11 +0200

    dich

--- first.py
+++ first.py
@@ -1,6 +1,11 @@
-print('heloohhghgd')
+print('hello')
 
 for i in range(10):
     print(f"my iteration {i}")
 
 
+print('I dont understand')
+t = 1.5
+while t <= 2.0:
+    print(f"my iteration {t}")
+    t = t from t
